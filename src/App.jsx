b323ebb1b86bf3d840677335ed1e92991af5a436
Dana Popa <popa.dana100@gmail.com>
2024-03-25 13:06:40 +0200

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import Box from './Box'
+import './App.css';
+import Box from './Box';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(100)
+
   return(
     <>
-      <Box name='Dana' title=' CTO' location='Helsinki'/>
-      <Box name='Karin' title=' Designer' location='Tartu'/>
-      <Box name='Kati' title=' Developer' location='Far away'/>
+    <p>Counter: {counter}</p>
+      <Box name=' Dana' title=' CTO' location=' Helsinki'/>
+      <Box name=' Karin' title=' Designer' location=' Tartu'/>
+      <Box name=' Kati' title=' Developer' location=' Far away'/>
     </>
   )
 }
